3585db43a31dd48e2553b5a0a71928746e996ecc
Fred Emmott <fred@fredemmott.co.uk>
2017-06-26 10:15:10 -0700

    Also test in PHP7 mode

--- .travis.sh
+++ .travis.sh
@@ -7,5 +7,9 @@ curl https://getcomposer.org/installer | hhvm -d hhvm.jit=0 --php -- /dev/stdin 
 
 cd /var/source
 hhvm -d hhvm.jit=0 /usr/local/bin/composer install
+
 hh_server --check $(pwd)
 hhvm -d hhvm.jit=0 vendor/bin/phpunit tests/
+if [ $(hhvm --php -r 'echo HHVM_VERSION_ID;' 2>/dev/null) -ge 32002 ]; then
+  hhvm -d hhvm.php7.all=1 -d hhvm.jit=0 vendor/bin/phpunit tests/
+fi
