6ea8fe2cf396a5e9f15d4ef1097ef4d9dcd15eaa
Fred Emmott <fe@fb.com>
2018-02-08 14:51:10 -0800

    update travis config

--- .travis.sh
+++ .travis.sh
@@ -3,16 +3,13 @@ set -ex
 hhvm --version
 
 composer install
-hhvm -d hhvm.jit=0 /usr/local/bin/composer install
 
 hh_server --check $(pwd)
-hhvm -d hhvm.jit=0 vendor/bin/phpunit tests/
+hhvm vendor/bin/phpunit tests/
 bin/hh-autoload
 hh_server --check $(pwd)
-hhvm -d hhvm.jit=0 vendor/bin/phpunit \
-  --bootstrap=vendor/hh_autoload.php tests/
-hhvm -d hhvm.jit=0 -d hhvm.php7.all=1 vendor/bin/phpunit \
+hhvm vendor/bin/phpunit \
   --bootstrap=vendor/hh_autoload.php tests/
 
-sed -i '/enable_experimental_tc_features/d' .hhconfig
+echo > .hhconfig
 hh_server --check $(pwd)
