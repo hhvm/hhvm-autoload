f691936e94333529f542d23dca48313b6d60efc2
Fred Emmott <fe@fb.com>
2018-02-08 14:43:15 -0800

    try and enable travis without docker

--- .travis.sh
+++ .travis.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -ex
 hhvm --version
-curl https://getcomposer.org/installer | hhvm -d hhvm.jit=0 --php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
 
-cd /var/source
+composer install
 hhvm -d hhvm.jit=0 /usr/local/bin/composer install
 
 hh_server --check $(pwd)
