94c8186c5011859a3d656e278a38af43d8633b81
Fred Emmott <fe@fb.com>
2018-06-12 13:00:30 -0700

    Use hh_client rather than hh_server in travis

--- .travis.sh
+++ .travis.sh
@@ -4,10 +4,10 @@ hhvm --version
 
 composer install
 
-hh_server --check $(pwd)
+hh_client
 hhvm vendor/bin/phpunit tests/
 bin/hh-autoload
-hh_server --check $(pwd)
+hh_client
 hhvm vendor/bin/phpunit \
   --bootstrap=vendor/hh_autoload.php tests/
 
