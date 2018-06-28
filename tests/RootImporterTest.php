3acf2165fd1f53ba50d853d8198d3a7724bd04a0
Karoun Kasraie <contact@karoun.me>
2018-06-27 17:21:09 -0700

    Revert "Fixing outdated test assertion; dev requirements should not be loaded in PROD_ONLY mode"
    This reverts commit e0ddcd62229e0c3d25d314a2ebe83b256385e7ec.

--- tests/RootImporterTest.php
+++ tests/RootImporterTest.php
@@ -20,7 +20,7 @@ final class RootImporterTest extends BaseTestCase {
       \array_keys($map['class']),
     );
 
-    $this->assertNotContains(
+    $this->assertContains(
       'phpunit_framework_testcase',
       \array_keys($map['class']),
     );
