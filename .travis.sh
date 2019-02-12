69f52b36de01eba942cc1428b33e08c679bc61e3
Fred Emmott <fe@fb.com>
2019-02-12 13:04:57 -0800

    Test in docker instead

--- .travis.sh
+++ .travis.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 set -ex
+apt update -y
+DEBIAN_FRONTEND=noninteractive apt install -y php-cli
 hhvm --version
+php --version
 
+(
+  cd $(mktemp -d)
+  curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+)
 composer install
 
 bin/hh-autoload
+cat vendor/autoload.hack
 hh_client
 hhvm vendor/bin/hacktest tests/*.php
 ENABLE_HH_CLIENT_AUTOLOAD=true hhvm vendor/bin/hacktest \
